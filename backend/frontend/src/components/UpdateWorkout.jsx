c14bf3419912f653a857ee13f38ed1bac51f6108
9024454 <9024454@student.zadkine.nl>
2026-03-18 12:12:50 +0100

    Opdracht 08 - Frontend

--- backend/frontend/src/components/UpdateWorkout.jsx
+++ backend/frontend/src/components/UpdateWorkout.jsx
@@ -26,7 +26,7 @@ function UpdateWorkout({ workoutId, currentTitle, currentReps, currentLoad, refr
       const response = await fetch(`http://127.0.0.1:4000/api/workouts/${workoutId}`, {
         method: 'PATCH',
         headers: {
-          'Content-Type': 'application/json'
+       
         },
         body: JSON.stringify(updatedWorkout)
       });
